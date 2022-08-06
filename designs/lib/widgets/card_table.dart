648ce4badace7f086f246ed0929d0579326875b7
Krasimir Zorbov <krasizorbov@abv.bg>
2022-08-06 20:21:19 +0300

    added_blur_effect

--- designs/lib/widgets/card_table.dart
+++ designs/lib/widgets/card_table.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CardTable extends StatelessWidget {
@@ -46,26 +48,34 @@ class _SingleCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15),
-      height: 180,
-      decoration: BoxDecoration(
-        color: const Color.fromRGBO(62, 66, 107, 0.7),
-        borderRadius: BorderRadius.circular(20)),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, 
-          children: [
-            CircleAvatar(
-              backgroundColor: color,
-              radius: 40,
-              child: Icon(icon, size: 50,),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            height: 180,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(62, 66, 107, 0.7),
+              borderRadius: BorderRadius.circular(20)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center, 
+                children: [
+                  CircleAvatar(
+                    backgroundColor: color,
+                    radius: 40,
+                    child: Icon(icon, size: 50,),
+                ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                text,
+                style: TextStyle(color: color, fontSize: 18),
+              )
+            ]),
           ),
-        const SizedBox(
-          height: 20,
         ),
-        Text(
-          text,
-          style: TextStyle(color: color, fontSize: 18),
-        )
-      ]),
+      ),
     );
   }
 }
