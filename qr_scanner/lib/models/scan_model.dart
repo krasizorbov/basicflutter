f8680b2b67cb1c082221314d5ab6aef0d9461959
Krasimir Zorbov <krasizorbov@abv.bg>
2022-08-10 15:41:17 +0300

    added_qr_scanner_config

---
+++ qr_scanner/lib/models/scan_model.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+ScanModel scanModelFromJson(String str) => ScanModel.fromJson(json.decode(str));
+
+String scanModelToJson(ScanModel data) => json.encode(data.toJson());
+
+class ScanModel {
+  ScanModel({
+    this.id,
+    this.type,
+    required this.value,
+  }) {
+    value.contains('http') ? type = 'http' : type = 'geo';
+  }
+
+  int? id;
+  String? type;
+  String value;
+
+  factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+        id: json["id"],
+        type: json["type"],
+        value: json["value"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "type": type,
+        "value": value,
+      };
+}
--- qr_scanner/lib/widgets/scann_button.dart
+++ qr_scanner/lib/widgets/scann_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScannButton extends StatelessWidget {
   const ScannButton({Key? key}) : super(key: key);
@@ -7,7 +8,15 @@ class ScannButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       elevation: 0,
-      child: const Icon(Icons.filter_center_focus), onPressed: () {},
+      child: const Icon(Icons.filter_center_focus), onPressed: () async {
+        //String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancel', false, ScanMode.QR );
+        const barcodeScanRes = 'https://fernando-herrera.com';
+        // final barcodeScanRes = 'geo:45.287135,-75.920226';
+
+        if ( barcodeScanRes == '-1' ) {
+          return;
+        }
+      },
     );
   }
 }
