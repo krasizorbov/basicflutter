ad751dbe67e2fd6497a75dd52c6280385182cec7
Krasimir Zorbov <krasizorbov@abv.bg>
2022-08-15 13:14:16 +0300

    update_product

--- products_app/lib/screens/product_screen.dart
+++ products_app/lib/screens/product_screen.dart
@@ -30,6 +30,7 @@ class _ProductScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productForm = Provider.of<ProductFormProvider>(context);
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -71,7 +72,10 @@ class _ProductScreenBody extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {}, // ToDo saving logic to firebase
+        onPressed: () async {
+          if (!productForm.isValidForm()) return;
+          await productService.saveOrCreateProduct(productForm.product);
+        }, // ToDo saving logic to firebase
         child: const Icon(
           Icons.save_as_outlined,
         ),
@@ -95,6 +99,8 @@ class _ProductForm extends StatelessWidget {
         width: double.infinity,
         decoration: _boxDecoration(),
         child: Form(
+          key: productForm.formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
@@ -120,7 +126,8 @@ class _ProductForm extends StatelessWidget {
                 TextFormField(
                   initialValue: product.price.toString(),
                   inputFormatters: [
-                    FilteringTextInputFormatter.allow(RegExp(r'^(\d+)?\.?\d{0,2}'))
+                    FilteringTextInputFormatter.allow(
+                        RegExp(r'^(\d+)?\.?\d{0,2}'))
                   ],
                   onChanged: (value) {
                     if (double.tryParse(value) != null) {
@@ -137,11 +144,12 @@ class _ProductForm extends StatelessWidget {
                   height: 30,
                 ),
                 SwitchListTile.adaptive(
-                    title: const Text('Available'),
-                    activeColor: Colors.indigo,
-                    value: product.available,
-                    onChanged: productForm.updateAvailability,// Implement switch logic
-                    ),
+                  title: const Text('Available'),
+                  activeColor: Colors.indigo,
+                  value: product.available,
+                  onChanged:
+                      productForm.updateAvailability, // Implement switch logic
+                ),
                 const SizedBox(
                   height: 30,
                 ),
--- products_app/lib/services/products_service.dart
+++ products_app/lib/services/products_service.dart
@@ -6,8 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:products_app/models/models.dart';
 
 class ProductsService extends ChangeNotifier {
-
-  final String _baseUrl = 'product-app-b8559-default-rtdb.europe-west1.firebasedatabase.app';
+  final String _baseUrl =
+      'product-app-b8559-default-rtdb.europe-west1.firebasedatabase.app';
   final List<Product> products = [];
   late Product selectedProduct;
 
@@ -21,19 +21,18 @@ class ProductsService extends ChangeNotifier {
   }
 
   Future<List<Product>> loadProducts() async {
-
     isLoading = true;
     notifyListeners();
-    
-    final url = Uri.https( _baseUrl, 'products.json');
-    final resp = await http.get( url );
 
-    final Map<String, dynamic> productsMap = json.decode( resp.body );
+    final url = Uri.https(_baseUrl, 'products.json');
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> productsMap = json.decode(resp.body);
 
     productsMap.forEach((key, value) {
-      final tempProduct = Product.fromMap( value );
+      final tempProduct = Product.fromMap(value);
       tempProduct.id = key;
-      products.add( tempProduct );
+      products.add(tempProduct);
     });
 
     isLoading = false;
@@ -42,82 +41,77 @@ class ProductsService extends ChangeNotifier {
     return products;
   }
 
-  Future saveOrCreateProduct( Product product ) async {
-
+  Future saveOrCreateProduct(Product product) async {
     isSaving = true;
     notifyListeners();
 
-    if ( product.id == null ) {
+    if (product.id == null) {
       // Create
-      await createProduct( product );
+      await createProduct(product);
     } else {
       // Update
-      await updateProduct( product );
+      await updateProduct(product);
     }
 
     isSaving = false;
     notifyListeners();
   }
-  
 
-  Future<String> updateProduct( Product product ) async {
+  Future<String> updateProduct(Product product) async {
+    final url = Uri.https(_baseUrl, 'products/${product.id}.json');
+    // ignore: unused_local_variable
+    final resp = await http.put(url, body: product.toJson());
+    // final decodedData = resp.body;
 
-    final url = Uri.https( _baseUrl, 'products/${ product.id }.json');
-    final resp = await http.put( url, body: product.toJson() );
-    final decodedData = resp.body;
-
-    //TODO: Update list of products
-    final index = products.indexWhere((element) => element.id == product.id );
+    final index = products.indexWhere((element) => element.id == product.id);
     products[index] = product;
 
     return product.id!;
   }
 
-  Future<String> createProduct( Product product ) async {
-
-    final url = Uri.https( _baseUrl, 'products.json');
-    final resp = await http.post( url, body: product.toJson() );
-    final decodedData = json.decode( resp.body );
+  Future<String> createProduct(Product product) async {
+    final url = Uri.https(_baseUrl, 'products.json');
+    final resp = await http.post(url, body: product.toJson());
+    final decodedData = json.decode(resp.body);
 
     product.id = decodedData['name'];
     products.add(product);
 
     return product.id!;
   }
-  
-  void updateSelectedProductImage( String path ) {
 
+  void updateSelectedProductImage(String path) {
     selectedProduct.picture = path;
-    newPictureFile = File.fromUri( Uri(path: path) );
+    newPictureFile = File.fromUri(Uri(path: path));
     notifyListeners();
   }
 
   Future<String?> uploadImage() async {
-
-    if (  newPictureFile == null ) return null;
+    if (newPictureFile == null) return null;
     isSaving = true;
     notifyListeners();
     // Get cloudinary credentials
     final url = Uri.parse('');
 
-    final imageUploadRequest = http.MultipartRequest('POST', url );
+    final imageUploadRequest = http.MultipartRequest('POST', url);
 
-    final file = await http.MultipartFile.fromPath('file', newPictureFile!.path );
+    final file =
+        await http.MultipartFile.fromPath('file', newPictureFile!.path);
 
     imageUploadRequest.files.add(file);
 
     final streamResponse = await imageUploadRequest.send();
     final resp = await http.Response.fromStream(streamResponse);
 
-    if ( resp.statusCode != 200 && resp.statusCode != 201 ) {
+    if (resp.statusCode != 200 && resp.statusCode != 201) {
       print('algo salio mal');
-      print( resp.body );
+      print(resp.body);
       return null;
     }
 
     newPictureFile = null;
 
-    final decodedData = json.decode( resp.body );
+    final decodedData = json.decode(resp.body);
     return decodedData['secure_url'];
   }
 }
